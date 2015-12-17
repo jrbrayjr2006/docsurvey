988ce39cf1c935b99d3fea1d315febff8ab04083
James Bray <jrbrayjr2006@gmail.com>
2015-12-16 20:47:48 -0600

    Change baseUrl to docsatisfaction.com

--- js/docsurvey.js
+++ js/docsurvey.js
@@ -5,9 +5,10 @@ console.debug("Entering application...");
 
 
 var app = angular.module('docsurveyApp', []);
+//Chnage the baseUrl value based on the environment
 //var baseUrl = 'http://www.jaydot2.com:8080/spring-survey-1.0.0-BUILD-SNAPSHOT';
-var baseUrl = 'http://www.strawberry23.net:8080/spring-survey-1.0.0-BUILD-SNAPSHOT';
-//var baseUrl = 'http://www.docsatisfaction.com:8080/spring-survey-1.0.0-BUILD-SNAPSHOT';
+//var baseUrl = 'http://www.strawberry23.net:8080/spring-survey-1.0.0-BUILD-SNAPSHOT';
+var baseUrl = 'http://www.docsatisfaction.com:8080/spring-survey-1.0.0-BUILD-SNAPSHOT';
 //var baseUrl = 'http://localhost:8080/spring';
 
 app.service('surveyService', function($http, $q, $log) {
